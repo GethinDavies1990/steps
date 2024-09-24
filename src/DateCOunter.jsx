9ffeee6b42c28f29c8fc8a466020a98459a9e081
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-09-24 14:46:12 +0100

    step complete

--- src/DateCOunter.jsx
+++ src/DateCOunter.jsx
@@ -5,23 +5,40 @@ import "./index.css";
 function DateCounter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
 
   return (
     <>
       <div className='button-container'>
-        <button onClick={() => setCount((currentCount) => currentCount - 1)}>
+        <button onClick={() => setStep((currentStep) => currentStep - 1)}>
           -
         </button>
-        <p>Count: {count}</p>
-        <button onClick={() => setCount((currentCount) => currentCount + 1)}>
+        <p>Step: {step}</p>
+        <button onClick={() => setStep((currentStep) => currentStep + 1)}>
           +
         </button>
       </div>
-      <br></br>
       <div className='button-container'>
-        <button>-</button>
-        <p>Step: {step}</p>
-        <button>+</button>
+        <button onClick={() => setCount((currentCount) => currentCount - step)}>
+          -
+        </button>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount((currentCount) => currentCount + step)}>
+          +
+        </button>
+      </div>
+      <div>
+        <p>
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+          </span>
+          <span>{date.toDateString()}</span>
+        </p>
       </div>
     </>
   );
